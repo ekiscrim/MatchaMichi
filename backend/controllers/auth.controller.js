1683cfcee8db9a8de10060cce3f0828bfc46448c
ekiscrim <xcstudiosonly@gmail.com>
2024-12-21 14:50:09 +0100

    2. - API User CRUD

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,97 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js"
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.json({
-        data: "Hi from the signup controller"
-    });
-}
+   try {
+    const {username, email, password} = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const exitingUser = await User.findOne({ username });
+    if (exitingUser) {
+        return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({ error: "Password must be at least 6 characters long" });
+    }
+
+    const exitingEmail = await User.findOne({ email });
+    if (exitingEmail) {
+        return res.status(400).json({ error: "Email is already taken" });
+    }    
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+        username,
+        email,
+        password:hashedPassword
+    })
+
+    if (newUser) {
+        generateTokenAndSetCookie(newUser._id, res);
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email
+        })
+    } else {
+        res.status(400).json({error: "Invalid user data"});
+    }
+
+   } catch (error) {
+        res.status(500).json({error: "Internal Server Error"});
+   }
+};
 
 export const login = async (req, res) => {
-    res.json({
-        data: "Hi from the login controller"
-    });
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user ||!isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        })
+
+    } catch (error) {
+        console.log("Error in login controller ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
 
 export const logout = async (req, res) => {
-    res.json({
-        data: "Hi from the logout controller"
-    });
+    try {
+        res.cookie("jwt","",{maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in getMe controller ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
