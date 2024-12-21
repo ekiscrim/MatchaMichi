8fd871a07210681e7dc09a19a397dee0f14dc444
ekiscrim <xcstudiosonly@gmail.com>
2024-12-21 12:14:03 +0100

    1. - Project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "Hi from the signup controller"
+    });
+}
+
+export const login = async (req, res) => {
+    res.json({
+        data: "Hi from the login controller"
+    });
+}
+
+export const logout = async (req, res) => {
+    res.json({
+        data: "Hi from the logout controller"
+    });
+}
